fe5b2223f263cbbc554a58aa1c70f668f1f796ee
Júlio Cézar <jczrpereira77@gmail.com>
2024-02-03 16:56:06 -0300

    fix: ensure first available day to book a service is tomorrow

--- src/app/barbershops/[id]/components/service-item.tsx
+++ src/app/barbershops/[id]/components/service-item.tsx
@@ -9,8 +9,8 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet'
-import { Barbershop, Service } from '@prisma/client'
-import { format, setHours, setMinutes } from 'date-fns'
+import { Barbershop, Booking, Service } from '@prisma/client'
+import { addDays, format, setHours, setMinutes } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import { signIn, useSession } from 'next-auth/react'
 import Image from 'next/image'
@@ -170,7 +170,7 @@ export function ServiceItem({ service, isAuthenticated }: ServiceItemProps) {
                       selected={date}
                       onSelect={handleDateClick}
                       locale={ptBR}
-                      fromDate={new Date()}
+                      fromDate={addDays(new Date(), 1)}
                       styles={{
                         head_cell: {
                           width: '100%',
