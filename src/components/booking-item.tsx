d5eb28a53d41b15f7fefaa887fb79f16514bb408
Júlio Cézar <jczrpereira77@gmail.com>
2024-02-25 13:33:01 -0300

    feat: add accessibility for datetime on booking-item component

--- src/components/booking-item.tsx
+++ src/components/booking-item.tsx
@@ -93,7 +93,15 @@ export function BookingItem({ booking }: BookingItemProps) {
                 })}
               </p>
               <p className="text-2xl">{format(booking.date, 'dd')}</p>
-              <p className="text-sm">{format(booking.date, 'hh:mm')}</p>
+              <time
+                className="text-sm"
+                title={format(booking.date, "dd 'de' MMMM 'ás' hh:mm", {
+                  locale: ptBR,
+                })}
+                dateTime={booking.date.toISOString()}
+              >
+                {format(booking.date, 'hh:mm')}
+              </time>
             </div>
           </CardContent>
         </Card>
